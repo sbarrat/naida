b71fd26e1a240fd33bba400a124a9a41f87e48bb
Ruben Lacasa <ruben@ensenalia.com>
2013-12-03 13:37:43 +0100

    Corregida etiqueta php en backend

--- backend.php
+++ backend.php
@@ -1,6 +1,6 @@
-<? //backend.php fichero de pie en el que sale toda la retalia ?>
+<?php //backend.php fichero de pie en el que sale toda la retalia ?>
 <font face='arial' size='-1' color='#BBBBBB'>
 Gesti&oacute;n clientas Naida - 
 Realizado &copy; <?php echo date('Y'); ?> <a href='http://rubenlacasa.es'>http://rubenlacasa.es</a> 
-para <a href='http://www.ensenalia.com'>Enseñalia</a>
+para <a href='http://www.ensenalia.com'>EnseÃ±alia</a>
 </font>
